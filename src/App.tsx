773123be31132007c090707b1683f828f8191c4c
Ericlys Moreira <ericlysm@gmail.com>
2023-09-13 08:18:33 -0300

    chore: chatcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import './App.css'
+import { Button } from "./components/ui/button"
 
 export function App() {
 
-  return (<h1>Hello world</h1>)
+  return (
+    <Button>Teste</Button>
+  )
 }
 
 export default App
